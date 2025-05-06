47938c64a87c4be5f3a25daba99a936e07e40627
hai <hai>
2025-05-06 11:42:33 +0900

    Fix login: LIMIT 1 + logic kiểm tra mật khẩu

--- hash.js
+++ hash.js
@@ -1,11 +1,5 @@
 const bcrypt = require('bcrypt');
-
 const plainPassword = '123456';
-
-bcrypt.hash(plainPassword, 10)
-  .then(hash => {
-    console.log('Mật khẩu đã mã hoá:', hash);
-  })
-  .catch(err => {
-    console.error('Lỗi khi hash mật khẩu:', err);
-  });
+bcrypt.hash(plainPassword, 10).then(hash => {
+  console.log('Hash:', hash);
+});

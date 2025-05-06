881751e59a8eb66b093892d72ba06bda2e3bc9a0
hai <hai>
2025-05-06 20:50:58 +0900

    Xóa route upload vì không dùng nữa

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const verifyToken = require("../middleware/verifyToken");
 const multer = require('multer');
 const path = require('path');
 const verify = require('../middleware/auth'); // ✅ phải có dòng này

c9c3cbada831b4d1bd2c28a0db05d8ca4b85794f
hai <hai>
2025-05-06 17:19:21 +0900

    move uploads out of backend and fix static path

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.listen(port, () => {
 
 // Route mọi request khác tới file index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'home.html'));
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
 
